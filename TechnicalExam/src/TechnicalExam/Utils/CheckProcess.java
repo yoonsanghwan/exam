a2933543e4c12252a712cce7571b625ecedf54a5
yoon <yoon@it-tera.com>
2016-06-17 11:46:27 +0900

    正規表現追加

--- TechnicalExam/src/TechnicalExam/Utils/CheckProcess.java
+++ TechnicalExam/src/TechnicalExam/Utils/CheckProcess.java
@@ -1,13 +1,21 @@
 package TechnicalExam.Utils;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class CheckProcess {
 
 	public static String findProcessName(String data) {
 
-		String processNmae = "";
-
 		System.out.println("data = " + data);
 		
+		String processNmae = "";
+		Pattern p = Pattern.compile("[0-9]");
+		Matcher m = p.matcher(data);
+		
+		System.out.println("matcher M  = " + m.find());
+		
+
 		return processNmae;
 	}
 
