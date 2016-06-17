6e6f73c29cca2310da0afa6c7306d0344b59a5c5
yoon <yoon@it-tera.com>
2016-06-17 11:23:13 +0900

    構造作成

--- TechnicalExam/src/TechnicalExam/MainProcess.java
+++ TechnicalExam/src/TechnicalExam/MainProcess.java
@@ -7,7 +7,17 @@ public class MainProcess {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Process start....");
-
+		String inputData = "123456435472848";
+		
+		if (inputData.equals("") || inputData == null) {
+			System.out.println("パラメータが入力されていません。");
+			return;
+		}
+		
+		//　@ TODO パラメータの数をチェックする
+		String subProcessName = CheckProcess.findProcessName(inputData);
+		
+		
 	}
 
 }
--- TechnicalExam/src/TechnicalExam/Utils/CheckProcess.java
+++ TechnicalExam/src/TechnicalExam/Utils/CheckProcess.java
@@ -2,4 +2,13 @@ package TechnicalExam.Utils;
 
 public class CheckProcess {
 
+	public static String findProcessName(String data) {
+
+		String processNmae = "";
+
+		System.out.println("data = " + data);
+		
+		return processNmae;
+	}
+
 }
